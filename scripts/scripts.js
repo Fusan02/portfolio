b16bced710080e7c29f452411b70cc543c2c599f
Fusan02 <saitoufutoshi0909@gmail.com>
2025-09-29 23:37:34 +0900

    add: f4samuraiインターンシップのwork追加
    add: work4のハッカソン作品の詳細を追加
    
    add: 個人開発ポータルサイトの詳細を追加

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+// おみくじ
 function Omikuji() {
   let fortune = ["大吉", "中吉", "小吉", "吉", "凶"];
   let random = Math.floor(Math.random() * fortune.length); //0.0~0.9 * 5 (0~4の範囲)
@@ -17,3 +18,45 @@ function Omikuji() {
     resultElement.style.color = "black";
   }
 }
+
+// ボタンでworksを非表示
+document.querySelectorAll('.toggle-works-btn').forEach(button => {
+  button.addEventListener('click', function() {
+    const targetNum = this.dataset.target
+    const targetWork = document.getElementById(`work_${targetNum}`);
+
+    // コンテンツの現在の表示状態を確認
+    if (targetWork.style.display == 'none') {
+      targetWork.style.display = '';
+      this.textContent = `work${targetNum}を非表示`;
+    } else {
+      targetWork.style.display = 'none';
+      this.textContent = `work${targetNum}を表示`;
+    }
+  });
+
+  button.textContent = `work${button.dataset.target}を非表示`
+})
+
+// ボタンで作成箇所の非表示
+const toggleHighlightsBtn = document.querySelector('.toggle-highlights-btn');
+const highlights = document.querySelectorAll('[class^="highlight-"');
+
+// 初期状態の設定
+let highlightsVisible = true;
+
+toggleHighlightsBtn.addEventListener('click', function() {
+  if (highlightsVisible) {
+    highlights.forEach(element => {
+      element.style.display = 'none';
+    });
+    this.textContent =' 担当箇所を表示';
+    highlightsVisible = false;
+  } else {
+    highlights.forEach(element => {
+      element.style.display = 'block';
+    });
+    this.textContent = '担当箇所を非表示';
+    highlightsVisible = true;
+  }
+});
